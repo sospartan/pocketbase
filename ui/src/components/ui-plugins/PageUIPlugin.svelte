98b2a36db4f1e1a3a229ab98899a3994be6c0afa
sospartan <sospartan@apache.org>
2025-07-14 15:05:52 +0800

    Implement collection management in PageUIPlugin with iframe communication and upsert panel integration

--- ui/src/components/ui-plugins/PageUIPlugin.svelte
+++ ui/src/components/ui-plugins/PageUIPlugin.svelte
@@ -2,20 +2,76 @@
     
     import { location } from "svelte-spa-router";
     import { pageTitle } from "@/stores/app";
+    import {
+        collections,
+        loadCollections
+    } from "@/stores/collections";
+    import CollectionUpsertPanel from "@/components/collections/CollectionUpsertPanel.svelte";
 
     $pageTitle = "plugin page";
+
+    let collectionUpsertPanel;
+    let iframe;
+
+    function listenIframe() {
+        window.addEventListener("message", (event) => {
+            
+            const { data } = event;
+            const { source, action} = data;
+            if(source.startsWith("ui-plugin/")) {
+                if (action === "edit-collection") {
+                    const { id } = data;
+                    showCollectionUpsertPanel(id);
+                }
+            }
+            
+        });
+    }
+
+    async function showCollectionUpsertPanel(id) {
+        if (!$collections) {
+            await loadCollections();
+        }
+
+        const collection = $collections.find((c) => c.id == id);
+        if (collection) {
+            collectionUpsertPanel.show(collection);
+        }
+    }
+
+    function notifyChanged() {
+        loadCollections();
+        iframe.contentWindow.postMessage({
+            source: "ui-plugin-host",
+            action: "changed",
+        }, "*");
+    }
+
+    
+    listenIframe();
+    loadCollections();
 
 </script>
 
 <div class="page-wrapper">
     {#if $location}
     <iframe
+        bind:this={iframe}
         src={$location}
         title="Plugin Manager"
         class="plugin-iframe"
     />
     {/if}
 </div>
+<CollectionUpsertPanel
+    bind:this={collectionUpsertPanel}
+    on:delete={() => {
+        notifyChanged()
+    }}
+    on:save={() => {
+        notifyChanged()
+    }}
+/>
 
 <style lang="scss">
     .plugin-iframe {
